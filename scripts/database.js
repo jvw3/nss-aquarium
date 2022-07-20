b32711e205c45512c7ff88e24712cb29d41b7066
Jonathan Woodard <jvw.226@gmail.com>
2022-07-20 11:41:28 -0500

    made layout changes. added scrollbar. added html function outuput.

--- scripts/database.js
+++ scripts/database.js
@@ -5,7 +5,41 @@ export const getFish = () => {
 const database = {
     fish: [
         {
-            name:
-        }
+            species: 'Salmon',
+            length: 1.5,
+            name: "Herbert",
+            harvestLocation: "Percy Priest Lake",
+            fishFood: "worms, herring, and laternfish"
+
+        },
+        {
+            species: 'Bluegill',
+            length: 3,
+            name: "Amy",
+            harvestLocation: "Cumberland River",
+            fishFood: "worms, herring, and laternfish"
+        },
+        {
+            species: 'Sturgeon',
+            length: 2.3,
+            name: "Reginald",
+            harvestLocation: "Old Hickory Lake",
+            fishFood: "worms, herring, and laternfish"
+        },
+        {
+            species: 'Sturgeon',
+            length: 1.5,
+            name: "Lebron",
+            harvestLocation: "Percy Priest Lake",
+            fishFood: "worms, herring, and laternfish"
+        },
+        {
+            species: 'Sturgeon',
+            Length: 1.5,
+            name: "Saquon",
+            harvestLocation: "Percy Priest Lake",
+            fishFood: "worms, herring, and laternfish"
+        },
     ]
 }
+
--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,18 @@ const allFish = getFish()
 for (const fish of allFish) {
     console.log(fish)
 }
+
+// Import the FishList function from the correct module
+import { FishList } from './fishlist.js'
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector(".allfish")
+
+
+parentHTMLElement.innerHTML = FishList()
